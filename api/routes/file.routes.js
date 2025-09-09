616269dbb9738e4d4adf13f240540f93a5b194d4
Rohit Bhatt <bhattrohed@gmail.com>
2025-09-09 18:13:40 +0530

    fixed api folder

--- api/routes/file.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadFile } = require('../controllers/file.controller');
-const upload = require('../middleware/multer.middleware');
-
-// The 'upload.single()' middleware processes the file before it hits the controller
-router.post('/upload', upload.single('medicalFile'), uploadFile);
-
-module.exports = router;
